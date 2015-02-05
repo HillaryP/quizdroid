f69d081465879f4aa6cbc0ef725f431e236c0651
Hillary Prather <prathh@uw.edu>
2015-02-04 23:32:49 -0800

    Factored out summary redundancy

--- app/src/main/java/edu/washington/prathh/quizdroid/MainActivity.java
+++ app/src/main/java/edu/washington/prathh/quizdroid/MainActivity.java
@@ -44,28 +44,25 @@ public class MainActivity extends ActionBarActivity {
 
     public void getSummary(View v) {
         Log.i(ACTIVITY, "Button click");
-        Intent intent;
+        Intent intent = new Intent(this, GenericOverview.class);
         switch (v.getId()) {
             case R.id.mathButton:
                 Log.i(ACTIVITY, "Math Button pushed");
-                intent = new Intent(this, MathOverview.class);
-                startActivity(intent);
+                intent.putExtra("class", "Math");
                 break;
             case R.id.physicsButton:
                 Log.i(ACTIVITY, "Physics Button pushed");
-                intent = new Intent(this, PhysicsOverview.class);
-                startActivity(intent);
+                intent.putExtra("class", "Physics");
                 break;
             case R.id.marvelButton:
                 Log.i(ACTIVITY, "Marvel Button pushed");
-                intent = new Intent(this, MarvelOverview.class);
-                startActivity(intent);
+                intent.putExtra("class", "Marvel");
                 break;
             case R.id.puppyButton:
                 Log.i(ACTIVITY, "Puppy Button pushed");
-                intent = new Intent(this, PuppyOverview.class);
-                startActivity(intent);
+                intent.putExtra("class", "Puppies");
                 break;
         };
+        startActivity(intent);
     }
 }
